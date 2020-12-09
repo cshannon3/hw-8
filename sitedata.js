34407c469227604f5e9ff635ef50057d84646600
cshannon3 <conreshan@gmail.com>
2020-12-08 23:47:29 -0500

    switched

---
+++ sitedata.js
@@ -0,0 +1,84 @@
+import FarmImg from "../assets/images/farm.png";
+import GovtImg from "../assets/images/government.png";
+import ParkImg from "../assets/images/park.png";
+import PoliceImg from "../assets/images/police.png";
+import SchoolImg from "../assets/images/school.png";
+import ShopImg from "../assets/images/shop.png";
+
+const sitedata = {
+    "farm":{
+        "imgPath":FarmImg,
+        "linear":{
+            "youtubeUrl":"https://www.youtube.com/embed/HIRbJEYb2Yg",
+            "questions":"How do we make healthy food more accessible to poor people?"
+        },
+        "system":{
+            "youtubeUrl":"https://www.youtube.com/embed/gfCcI6_1iiA"
+        }
+    },
+    "police":{
+        "imgPath":PoliceImg,
+        "linear":{
+            "youtubeUrl":[
+                "https://www.youtube.com/embed/Wsq30E6OSVU",
+                "https://www.youtube.com/embed/g2sAi5So4EE"
+            ]
+        },
+        "system":{
+            "youtubeUrl":"https://www.youtube.com/embed/f4J1pgxYTww"
+            //Xv9qv0L-7kc
+        }
+
+    },
+    "school":{
+        "imgPath":SchoolImg,
+        "linear":{
+            "youtubeUrl":"https://www.youtube.com/embed/81Ub0SMxZQo"
+            //https://www.youtube.com/watch?v=81Ub0SMxZQo&t=552s&ab_channel=TED
+            //https://www.youtube.com/watch?v=dfWhWyAoUMY&t=2s&ab_channel=TeachForAmerica
+        },
+        "system":{
+            "youtubeUrl":"https://www.youtube.com/embed/HIRbJEYb2Yg"
+        }
+
+    },
+    "government":{
+        "imgPath":GovtImg,
+        "linear":{
+            "youtubeUrl":"https://www.youtube.com/embed/HIRbJEYb2Yg"
+        },
+        "system":{
+            "youtubeUrl":"https://www.youtube.com/embed/HIRbJEYb2Yg"
+        }
+
+    },
+    "park":{
+        "imgPath":ParkImg,
+        "linear":{
+            "youtubeUrl":"https://www.youtube.com/embed/HIRbJEYb2Yg"
+        },
+        "system":{
+            "youtubeUrl":"https://www.youtube.com/embed/HIRbJEYb2Yg"
+        }
+
+    },
+    "shop":{
+        "imgPath":ShopImg,
+        "linear":{
+            "youtubeUrl":"https://www.youtube.com/embed/HIRbJEYb2Yg"
+        },
+        "system":{
+            "youtubeUrl":"https://www.youtube.com/embed/HIRbJEYb2Yg"
+        }
+    },
+    "solar":{
+        "imgPath":ShopImg,
+        "linear":{
+            "youtubeUrl":"https://www.youtube.com/embed/HIRbJEYb2Yg"
+        },
+        "system":{
+            "youtubeUrl":"https://www.youtube.com/embed/6qqqVwM6bMM"
+        }
+    }
+}
+
